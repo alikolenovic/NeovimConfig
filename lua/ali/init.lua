d15186159e0d9387118f6ef227a51a8e2f3ec9af
alikolenovic <akolenov@stevens.edu>
2024-12-09 14:51:23 -0500

    Updates

--- lua/ali/init.lua
+++ lua/ali/init.lua
@@ -6,21 +6,21 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Diagnostic config for errors
 vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    float = {
-        source = true,
-        border = "rounded"
-    },
+  virtual_text = false,
+  signs = true,
+  float = {
+    source = true,
+    border = "rounded"
+  },
 })
 -- Floating window keybind
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>dt", function()
-    if vim.diagnostic.is_enabled() then
-        vim.diagnostic.enable(false)
-    else
-        vim.diagnostic.enable(true)
-    end
+  if vim.diagnostic.is_enabled() then
+    vim.diagnostic.enable(false)
+  else
+    vim.diagnostic.enable(true)
+  end
 end)
 
 require("ali.lazy_init")
